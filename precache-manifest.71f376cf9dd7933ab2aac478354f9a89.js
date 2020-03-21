8b33cde3f7c8313af40065cfcae387f8b8e943ef
gmmurray <trinilime123@live.com>
2020-03-21 10:32:43 -0400

    Updates

---
+++ precache-manifest.71f376cf9dd7933ab2aac478354f9a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51add94aaebc476dd07dc4b43a97782",
+    "url": "/mern-exercise-tracker/index.html"
+  },
+  {
+    "revision": "afef76e17486fe5511a8",
+    "url": "/mern-exercise-tracker/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "afef76e17486fe5511a8",
+    "url": "/mern-exercise-tracker/static/js/2.1283080c.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/mern-exercise-tracker/static/js/2.1283080c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249c710f88934c88569f",
+    "url": "/mern-exercise-tracker/static/js/main.672441ff.chunk.js"
+  },
+  {
+    "revision": "f47fc2288967a2d37e67",
+    "url": "/mern-exercise-tracker/static/js/runtime-main.4bf4cb81.js"
+  }
+]);
